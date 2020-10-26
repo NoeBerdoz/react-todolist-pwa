b187d1d7cd62177e5689b16377d5e1d020a76e1d
NoeBerdoz <44669187+NoeBerdoz@users.noreply.github.com>
2020-10-26 10:49:13 +0100

    Added animations library and filtered useless files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import './App.css';
 import './css/TaskList.css'
 import NewTaskForm from "./components/NewTaskForm";
 import TaskList from "./components/TaskList";
-
+import {useSpring, animated, useTransition} from "react-spring";
 
 function App() {
+
+    const props = useSpring({
+        to: async (next, cancel) => {
+            await next({opacity: 1, color: '#ffffee'})
+            await next({opacity: 1, color: '#61dafb'})
+        },
+        from: {opacity: 0, color: 'black'}
+    })
+
+    const ref = useRef([])
+    const [items, set] = useState([])
+    const transition = useTransition(items, null, {
+        from: { opacity: 0, height: 0, innerHeight: 0, transform: 'perspective(600px) rotateX(0deg)', color: '#8fa5b6' },
+        enter: [
+            { opacity: 1, height: 80, innerHeight: 80 },
+            { transform: 'perspective(600px) rotateX(180deg)', color: '#28d79f' },
+            { transform: 'perspective(600px) rotateX(0deg)' },
+        ],
+        leave: [{ color: '#c23369' }, { innerHeight: 0}, { opacity: 0, height: 0}],
+        update: {color: '#28b4d7' },
+    })
+
+    const reset = useCallback(() => {
+        ref.current.map(clearTimeout)
+        ref.current = []
+        set([])
+        ref.current.push(setTimeout(() => set(['Bienvenue', 'Tâches', 'Noé']), 2000))
+        ref.current.push(setTimeout(() => set(['Bienvenue', 'Noé']), 5000))
+        ref.current.push(setTimeout(() => set(['Bienvenue', 'Sur votre liste', 'Noé']), 8000))
+    }, [])
+
+    useEffect(() => void reset(), [])
+
   return (
       <div className="container">
-        <h1 className="title">Liste de tâches</h1>
+        <div>{transition.map(({ item, props: { innerHeight, ...rest }, key }) => (
+            <animated.div clasName="transition-item" key={key} style={rest} onClick={reset}>
+                <animated.div style={{ overflow: 'hidden', height: innerHeight }}>{item}</animated.div>
+            </animated.div>
+              ))}
+        </div>
+        <animated.div style={props}><h1 className="title">Liste de tâches</h1></animated.div>
         <NewTaskForm/>
         <TaskList/>
       </div>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import Task from "./Task";
 import { useTasks } from "../services/TaskProvider";
 
+
 export default function TaskList() {
+
     // Extract tasks
     const { tasks } = useTasks()
 
